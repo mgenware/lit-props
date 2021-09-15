4d83cc20c086e683fa3cbbd7e62a5c04e3e28140
Mgen <m@mgenware.com>
2021-09-15 20:06:35 +0800

    Update conf files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,11 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['airbnb-typescript-lite', 'plugin:@typescript-eslint/recommended', 'mgenware'],
+  extends: ['mgenware'],
   parserOptions: {
     sourceType: 'module',
     project: './tsconfig.json',
+  },
+  rules: {
+    'import/extensions': 'off',
   },
 };
